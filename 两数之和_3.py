31f27dfa471640d24c65fd2a1397629fbbde822b
swxfll <swxfll@outlook.com>
2022-11-09 19:07:07 +0800

    两书之和

--- 两数之和_3.py
+++ 两数之和_3.py
@@ -4,7 +4,8 @@ class Solution:
         for i in range(num):
             for j in range(i+1, num):
                 if nums[i] + nums[j] == target:
-                    return i, j
+                    return [i, j]
+        return []
 
 
 
